a080693509ede4d82abb9385e93e0514aebe1554
SiwonHae <705lo@naver.com>
2023-11-04 23:14:06 +0900

    add getter

--- src/main/java/com/oss2/dasom/domain/BaseTimeEntity.java
+++ src/main/java/com/oss2/dasom/domain/BaseTimeEntity.java
@@ -3,6 +3,7 @@ package com.oss2.dasom.domain;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
