ec97cde9a5b1fbe602b603f3f91b6cb7a614c141
SiwonHae <705lo@naver.com>
2023-11-04 23:27:07 +0900

    add find userinfo

--- src/main/java/com/oss2/dasom/repository/UserRepository.java
+++ src/main/java/com/oss2/dasom/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.oss2.dasom.repository;
 
+import com.oss2.dasom.domain.NanoId;
 import com.oss2.dasom.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+    User findByUserId(NanoId userId);
 }
