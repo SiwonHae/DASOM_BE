d119a22b5d04b531cc37841c979a01503e564998
linavell <linavell@naver.com>
2023-11-15 02:38:01 +0900

    feat: 내가 작성한 미팅 게시물 조회, 성별 필터 조회 구현

--- src/main/java/com/oss2/dasom/controller/PostController.java
+++ src/main/java/com/oss2/dasom/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.oss2.dasom.controller;
 
+import com.oss2.dasom.domain.Gender;
 import com.oss2.dasom.domain.NanoId;
 import com.oss2.dasom.domain.Post;
 import com.oss2.dasom.dto.CreatePostRequest;
@@ -9,8 +10,6 @@ import com.oss2.dasom.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -41,9 +40,29 @@ public class PostController {
 
     }
 
+    // 특정 사용자가 작성한 게시물 조회
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<GetPostResponse>> findPostByUser(@PathVariable String userId) {
+        List<GetPostResponse> posts = postService.getPostUserId(userId)
+                .stream()
+                .map(GetPostResponse::new)
+                .toList();
+        return ResponseEntity.ok().body(posts);
+    }
+
+    // 성별 필터 조회
+    @GetMapping("/{gender}")
+    public ResponseEntity<List<GetPostResponse>> findPostByGender(@PathVariable Gender gender) {
+        List<GetPostResponse> posts = postService.getPostGender(gender)
+                .stream()
+                .map(GetPostResponse::new)
+                .toList();
+        return ResponseEntity.ok().body(posts);
+    }
+
     // 게시물 상세 조회
     @GetMapping("/{postId}")
-    public ResponseEntity<GetPostResponse> findPost(@PathVariable Long postId) {
+    public ResponseEntity<GetPostResponse> findPost(@PathVariable String postId) {
         GetPostResponse postResponse = new GetPostResponse(postService.getPostId(postId));
         return ResponseEntity.ok()
                 .body(postResponse);
@@ -58,14 +77,14 @@ public class PostController {
 
     // 게시물 수정
     @PutMapping("/{postId}")
-    public ResponseEntity<Void> updatePost(@PathVariable Long postId, @RequestBody UpdatePostRequest request) {
+    public ResponseEntity<Void> updatePost(@PathVariable String postId, @RequestBody UpdatePostRequest request) {
         postService.updatePost(postId, request);
         return ResponseEntity.ok().build();
     }
 
-    // postId로 삭제
+    // 게시물 삭제
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+    public ResponseEntity<Void> deletePost(@PathVariable String postId) {
         postService.deletePost(postId);
         return ResponseEntity.ok().build();
     }
